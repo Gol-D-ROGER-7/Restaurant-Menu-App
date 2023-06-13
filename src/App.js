23fc3fe649f138cde05f2ceedc17ba472a0daa68
Gol-D-ROGER-7 <shelbyarthur874@gmail.com>
2023-06-13 03:58:02 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+// import './App.css'
+import Resturant from './components/basics/Resturant'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Resturant/>
+    </div>
+  )
+}
+
+export default App
